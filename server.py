c857a3e20f56492b059c98a4ce9fb3e63d4ce292
Md. Asif Tarafder <110770643+Asif-Tarafder@users.noreply.github.com>
2023-10-01 17:54:14 +0600

    LAB03_Class task3
    server communicate with multiple client server

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 HEADER = 16
 PORT = 5050
@@ -12,29 +13,40 @@ server.bind(ADDR)
 
 server.listen()
 print('Server is listening.')
+def clients(conn,addr):
 
-conn, addr = server.accept()
-connection=True
+    connection = True
 
-while connection:
-    message_len = conn.recv(HEADER).decode(FORMAT)
-    if message_len:
-        message_len = int(message_len)
-        message = conn.recv(message_len).decode(FORMAT)
-        if message == DISCONNECT_MESSAGE:
-                connection =  False
-                conn.send('Connection Closed.'.encode(FORMAT))
-        else:
-            c=0
-            for i in message:
-                if i in "aeiouAEIOU":
-                    c+=1
-            
-            if c==0:
-                conn.send("Not enough vowels".encode(FORMAT))
-            elif c<=2:
-                conn.send("Enough vowels I guess".encode(FORMAT))
-            else:
-                conn.send("Too many vowels".encode(FORMAT))
-            conn.send('Message Received.'.encode(FORMAT))
-conn.close()
+    while connection:
+       message_len = conn.recv(HEADER).decode(FORMAT)
+       if message_len:
+           message_len = int(message_len)
+           message = conn.recv(message_len).decode(FORMAT)
+           if message == DISCONNECT_MESSAGE:
+                   connection =  False
+                   conn.send('Connection Closed.'.encode(FORMAT))
+           else:
+                c = 0
+                for i in message:
+                    if i in "aeiouAEIOU":
+                        c +=1
+                if c==0:
+                    conn.send("Not enough vowels".encode(FORMAT))
+                elif c<=2:
+                    conn.send("Enough vowels I guess".encode(FORMAT))
+                else:
+                    conn.send("Too many vowels".encode(FORMAT))
+                conn.send('Message Received.'.encode(FORMAT))
+    conn.close()
+
+while True:
+    conn, addr = server.accept()
+    thread = threading.Thread(target=clients, args=(conn, addr))
+    thread.start()
+    print(f'Clients Connected: {threading.active_count()-1}.')
+    
+    
+
+                
+                
+    
